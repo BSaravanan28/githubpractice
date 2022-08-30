6a791ec57038dee6fb128b5bb6d6ecba427297c0
Saravanan <Saravanan@LAPTOP-SNC2BPSD>
2022-08-30 11:06:08 +0530

    for pipeline

---
+++ src/test/java/Practice/TestPractice1.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class TestPractice1 {
+	
+	@Test (groups= {"smoke"})
+	public void test1()
+	{
+		System.out.println("Test1 is running");
+		
+	}
+
+}
---
+++ src/test/java/Practice/TestPractice2.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class TestPractice2 {
+	
+	@Test (groups= {"regression"})
+	public void test2()
+	{
+		System.out.println("Test2 is running");
+		
+	}
+}
